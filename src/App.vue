336e417d3db506259193871000f8e6dd07990785
Julius Pedersen <deifyed@tutanota.com>
2021-12-26 13:17:29 +0100

    ✅ Add header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <template>
-	<router-view></router-view>
+    <Header />
+
+    <router-view></router-view>
 </template>
 
 <script>
+import Header from '/src/components/Header.vue'
+
+export default {
+    components: {
+        Header,
+    },
+}
 </script>
 
 <style lang="scss">
 html,
-	body {
-margin: 0;
-padding:  0;
-	}
+body {
+    margin: 0;
+    padding: 0;
+}
 
 * {
-	box-sizing:  border-box;
+    box-sizing: border-box;
 }
 
 #app {
-	height: 100%;
-	width: 100%;
+    height: 100%;
+    width: 100%;
 
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Header">
+        <span>Motoblog</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '/src/assets/styling/theme.scss';
+
+.Header {
+    display: flex;
+
+    padding: 1em;
+
+    background-color: $primary-color;
+    color: $primary-text-color;
+
+    font-weight: bold;
+}
+</style>
