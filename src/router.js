1c248191de1e8d98b1f2c4fe16524adbccf9a075
Julius Pedersen <deifyed@tutanota.com>
2021-12-26 12:59:59 +0100

    ✅ Add redirect to dev post

--- src/router.js
+++ src/router.js
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PostDetailsView from '/src/features/post-details/PostDetailsView.vue'
 
 const routes = [
-	{ path: '/posts/:id', component: PostDetailsView },
+    { path: '/', redirect: '/posts/0082da27-ad07-46ca-a64c-9b1801e17916' },
+    { path: '/posts/:id', component: PostDetailsView },
 ]
 
 export const router = createRouter({
-	history: createWebHistory(),
-	routes,
+    history: createWebHistory(),
+    routes,
 })
-
