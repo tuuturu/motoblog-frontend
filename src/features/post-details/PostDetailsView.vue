191f4edd9ce92857091cd680c3b11059b3202abf
Julius Pedersen <deifyed@tutanota.com>
2021-12-27 09:38:02 +0100

    👌 Improve post design

--- src/features/post-details/PostDetailsView.vue
+++ src/features/post-details/PostDetailsView.vue
@@ -1,31 +1,72 @@
 <template>
-	<div class="PostDetailsView">
-    <h1>{{ post.title }}</h1>
-    <span>{{ post.time }}</span>
-    <p>{{ post.content }}</p>
-	</div>
+    <div class="PostDetailsView">
+        <div class="content">
+            <h1>{{ post.title }}</h1>
+
+            <span class="date-stamp">{{ dateStamp }}</span>
+
+            <p>{{ post.content }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-	name: 'PostDetailsView',
-	computed: {
-    post() {
-      return this.$store.state['posts'].posts.find(p => p.id === this.postID)
-    }
-	},
-  data: () => ({
-    postID: -1,
-  }),
-	async created() {
-		await this.$store.dispatch('posts/fetch')
+import { monthAsHuman } from './dates.js'
 
-		this.postID = this.$route.params.id
-	}
+export default {
+    name: 'PostDetailsView',
+    computed: {
+        post() {
+            return this.$store.state['posts'].posts.find(p => p.id === this.postID)
+        },
+        dateStamp() {
+            const time = new Date(this.post.time)
+
+            const year = time.getUTCFullYear()
+            const month = monthAsHuman(time.getMonth())
+            const day = time.getDate()
+
+            return `${day}. ${month} ${year}`
+        },
+    },
+    data: () => ({
+        postID: -1,
+    }),
+    async created() {
+        await this.$store.dispatch('posts/fetch')
+
+        this.postID = this.$route.params.id
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-.PostDetailsView {}
-</style>
+.PostDetailsView {
+    display: flex;
+    justify-content: center;
+}
 
+h1 {
+    margin-top: 2em;
+
+    font-size: 42pt;
+}
+
+.content {
+    width: 40%;
+
+    display: flex;
+    flex-direction: column;
+}
+
+.date-stamp {
+    align-self: flex-end;
+}
+
+p {
+    text-align: left;
+
+    font-size: 18pt;
+    line-height: 2em;
+}
+</style>
--- src/store/mockposts.js
+++ src/store/mockposts.js
@@ -1,8 +1,9 @@
 export default [
-	{
-		id: '0082da27-ad07-46ca-a64c-9b1801e17916',
-		title: 'Je Tau!',
-		time: '2006-08-14T02:34:56-06:00',
-		content: 'so much text',
-	},
+    {
+        id: '0082da27-ad07-46ca-a64c-9b1801e17916',
+        title: 'Je Tau!',
+        time: '2019-09-26T07:58:30.996+0200',
+        content:
+            'Finally reached France! Its been a effing ride. The heat has been relentless, but man. No rain for three whole days. Cant really complain',
+    },
 ]
