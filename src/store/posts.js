c53ef49c8405ef1d3c65ae421cdea3125d248c7c
Julius Pedersen <deifyed@tutanota.com>
2022-01-05 20:47:38 +0100

    ✅ Use actual backend posts

--- src/store/posts.js
+++ src/store/posts.js
@@ -1,33 +1,44 @@
-import mockPosts from './mockposts.js'
+import Axios from 'axios'
+
+import config from '/src/app.config'
+
+const axios = Axios.create({
+    baseURL: config['VITE_APP_POST_SERVICE_URL'],
+})
 
 const state = () => ({
-	posts: [],
+    posts: [],
 })
 
 const mutations = {
-	post(state, post) {
-		const index = state.posts.findIndex(p => p.id === post.id)
+    post(state, post) {
+        const index = state.posts.findIndex(p => p.id === post.id)
 
-		if(index !== -1) state.posts.splice(index, 1)
+        if (index !== -1) state.posts.splice(index, 1)
 
-		state.posts.push(post)
-	},
-	posts(state, posts) {
-		state.posts.splice(0, state.posts.length)
+        state.posts.push(post)
+    },
+    posts(state, posts) {
+        state.posts.splice(0, state.posts.length)
 
-		state.posts.push(...posts)
-	},
+        state.posts.push(...posts)
+    },
 }
 
 const actions = {
-	fetch({ commit }) {
-		commit('posts', mockPosts)
-	}
+    async fetch({ commit }) {
+        const { data } = await axios.request({
+            url: '/posts',
+            method: 'GET',
+        })
+
+        commit('posts', data)
+    },
 }
 
 export default {
-	namespaced: true,
-	state,
-	mutations,
-	actions,
+    namespaced: true,
+    state,
+    mutations,
+    actions,
 }
